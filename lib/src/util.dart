54376e6d00b9d70f307e22115e42d3eeba9bace3
Nick Stijger <56773819+NickStijger@users.noreply.github.com>
2023-09-18 20:23:10 +0200

    Update util.dart (#41)
    Previous regex pattern didn't match all valid formats;
    this one follows RFC 5322 which is more complete

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,6 +1,7 @@
 class RTUtils {
   /// Email Regex - A predefined type for handling email matching
-  static const emailPattern = r'\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b';
+  static const emailPattern = 
+    r'''(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])''';
 
   /// URL Regex - A predefined type for handling URL matching
   static const urlPattern =
