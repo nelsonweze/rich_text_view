b8b50471f27d025dd97cd8f92fba3fea21af5bb8
N.L.M. Stijger <Nick.Stijg@gmail.com>
2023-09-19 12:12:30 +0200

    improved phone pattern: https://regexr.com/3aevr

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -9,7 +9,7 @@ class RTUtils {
 
   /// Phone Regex - A predefined type for handling phone matching
   static const phonePattern =
-      r'(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})';
+      r'''((\+|00(\s|\s?\-\s?)?)31(\s|\s?\-\s?)?(\(0\)[\-\s]?)?|0)[1-9]((\s|\s?\-\s?)?[0-9])((\s|\s?-\s?)?[0-9])((\s|\s?-\s?)?[0-9])\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]''';
 
   static const boldPattern = r'\*.*?\*';
   static const hashPattern = r'\B#+([^\x00-\x7F]|\w)+';
